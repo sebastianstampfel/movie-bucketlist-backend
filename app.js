d979fc3c88a280b2933a91074bac6941985763ca
Sebastian Stampfel <ic19b084@technikum-wien.at>
2020-12-03 17:45:39 +0100

    Allow PUT in CORS

--- app.js
+++ app.js
@@ -59,8 +59,9 @@ app.use(express.json());
 */
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*") // Allow requests from everywhere
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+	res.header("Access-Control-Allow-Methods", "GET, PUT")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next()
 })
 
 /**
